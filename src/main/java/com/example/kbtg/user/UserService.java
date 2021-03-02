0f388d9aed0033bd5092610357a2ec08e1d0235b
Vorapoj Pitithanachaikul <vorapoj.p@kbtg.tech>
2021-03-02 15:52:11 +0700

    workshop 07

--- src/main/java/com/example/kbtg/user/UserService.java
+++ src/main/java/com/example/kbtg/user/UserService.java
@@ -1,15 +1,28 @@
 package com.example.kbtg.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
-    public UserResponse getInfo(int id) {
-        if(id <= 10) {
-            // Success
-            return new UserResponse(id, "somkiat", 30);
+
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserResponse getInfo(int id){
+        Optional<MyUser> user = userRepository.findById(id);
+        if (user.isPresent()){
+            MyUser myUser = user.get();
+            return new UserResponse(myUser.getId(), myUser.getName(), myUser.getAge());
+        }else{
+            throw new UserNotFoundException("User not found id: " + id);
         }
-        // Fail
-        throw new UserNotFoundException("User not found id="+ id);
     }
 }
